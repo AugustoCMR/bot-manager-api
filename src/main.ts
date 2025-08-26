b5fdd3b0d7f3c2772efe1c2384dbf51426735c2c
Augusto Ribeiro <augustoribeiro100.g.j@gmail.com>
2025-08-26 18:21:38 -0300

    chore: habilitando cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,

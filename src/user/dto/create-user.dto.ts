32d0bcb66141239efdd77046c81f467348b0f9c4
Augusto Ribeiro <augustoribeiro100.g.j@gmail.com>
2025-08-26 16:44:24 -0300

    feat: aplica ApiProperty para mapeamento do swagger

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,17 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
 import { IsEmail, IsNotEmpty, IsString, Length, MaxLength } from "class-validator";
 
 export class CreateUserDto {
+  @ApiProperty()
   @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
   @IsEmail({}, { message: "Email inválido"})
   @IsNotEmpty({message: "Email é obrigatório"})
   email:string;
 
+  @ApiProperty()
   @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
   @MaxLength(20, { message: 'Nome deve conter no máximo 200 caracteres' })
   @IsNotEmpty({message: "Nome é obrigatório"})
   name:string;
 
+  @ApiProperty()
   @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
   @Length(6, 30, { message: 'Senha deve conter entre 6 e 30 caracteres' })
   @IsNotEmpty({message: "Senha é obrigatório"})

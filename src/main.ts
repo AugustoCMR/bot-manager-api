7148a6ab31496d588c48c0ecc565b4937ea5a87b
Augusto Ribeiro <augustoribeiro100.g.j@gmail.com>
2025-08-26 16:44:04 -0300

    chore: configura o swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,42 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    })
+  );
+  const config = new DocumentBuilder()
+    .setTitle("bot-manager")
+    .setDescription("API para gerenciamento de bots")
+    .setVersion("1.0")
+    .addTag("bots")
+    .addBearerAuth()
+    .addGlobalResponse({
+      status: 500,
+      description: "Internal server error",
+    })
+    .build();
+  const customOptions: SwaggerCustomOptions = {
+    customSiteTitle: "bot-manager",
+    explorer: true
+  };
+  const documentOptions: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, documentOptions);
+  SwaggerModule.setup("api", app, documentFactory, customOptions);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
